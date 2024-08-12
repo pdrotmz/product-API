b2e1cabe02910ea8305ebce2c64396cbc44c7fba
pdrotmz <pedrotomaz0210@gmail.com>
2024-08-12 11:55:38 -0300

    Initial commit

---
+++ src/main/java/dev/pdrotomz/productSBA/ProductSbaApplication.java
@@ -0,0 +1,13 @@
+package dev.pdrotomz.productSBA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSbaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pdrotomz/productSBA/ProductSbaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pdrotomz.productSBA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
