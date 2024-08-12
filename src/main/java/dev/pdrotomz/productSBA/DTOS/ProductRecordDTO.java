23e97d406ad287e9c32452584dcb9e2293f1afc7
pdrotmz <pedrotomaz0210@gmail.com>
2024-08-12 20:03:14 -0300

    feat(DTO): add ProductRecordDTO record

---
+++ src/main/java/dev/pdrotomz/productSBA/DTOS/ProductRecordDTO.java
@@ -0,0 +1,9 @@
+package dev.pdrotomz.productSBA.DTOS;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDTO(@NotBlank String name, @NotNull BigDecimal value) {
+}
